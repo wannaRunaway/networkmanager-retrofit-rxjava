f953cde986cc39ee25b028726ed27a41cc55e3ee
xuedi <xuedicr7@gmail.com>
2021-10-17 09:53:38 +0800

    add jitpack.yml add openjdk 11

--- app/src/main/java/com/example/moduleupload/MainActivity.java
+++ app/src/main/java/com/example/moduleupload/MainActivity.java
@@ -5,10 +5,6 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.networkmanager_retrofitrxjava.InternetworkManager;
-import com.example.networkmanager_retrofitrxjava.MyBanner;
-import com.example.networkmanager_retrofitrxjava.MyObserver;
-
 import java.util.List;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
@@ -18,29 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        InternetworkManager.getRetrofitApi(this).getBannerList()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new MyObserver<List<MyBanner>>() {
-                    @Override
-                    public void onSuccess(List<MyBanner> data) {
 
-                    }
-
-                    @Override
-                    public void onFail(int code, String message) {
-
-                    }
-
-                    @Override
-                    public void onError() {
-
-                    }
-
-                    @Override
-                    public void onClearToken() {
-
-                    }
-                });
+//        InternetworkManager.getRetrofitApi(this).getBannerList()
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new MyObserver<List<MyBanner>>() {
+//                    @Override
+//                    public void onSuccess(List<MyBanner> data) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onFail(int code, String message) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onError() {
+//
+//                    }
+//
+//                    @Override
+//                    public void onClearToken() {
+//
+//                    }
+//                });
     }
 }
